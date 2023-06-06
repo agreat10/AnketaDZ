858c573f3c19464d9c2a66f521a440151ca52134
agreat10 <komolov10@gmail.com>
2023-06-06 18:49:46 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace AnketaDZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLname = new System.Windows.Forms.Label();
+            this.txtLname = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblNameCount = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.lblProv = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(12, 37);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(866, 104);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Список анкет:";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtName.Location = new System.Drawing.Point(17, 200);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(198, 30);
+            this.txtName.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblName.Location = new System.Drawing.Point(12, 161);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(54, 25);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Имя";
+            // 
+            // lblLname
+            // 
+            this.lblLname.AutoSize = true;
+            this.lblLname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblLname.Location = new System.Drawing.Point(237, 161);
+            this.lblLname.Name = "lblLname";
+            this.lblLname.Size = new System.Drawing.Size(103, 25);
+            this.lblLname.TabIndex = 1;
+            this.lblLname.Text = "Фамилия";
+            // 
+            // txtLname
+            // 
+            this.txtLname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtLname.Location = new System.Drawing.Point(242, 200);
+            this.txtLname.Name = "txtLname";
+            this.txtLname.Size = new System.Drawing.Size(198, 30);
+            this.txtLname.TabIndex = 2;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblEmail.Location = new System.Drawing.Point(455, 161);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(60, 25);
+            this.lblEmail.TabIndex = 1;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtEmail.Location = new System.Drawing.Point(460, 200);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(198, 30);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(686, 256);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(192, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Сохранить в файл .txt";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtPhone.Location = new System.Drawing.Point(680, 200);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(198, 30);
+            this.txtPhone.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAdd.Location = new System.Drawing.Point(17, 279);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(116, 42);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnChange
+            // 
+            this.btnChange.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnChange.Location = new System.Drawing.Point(217, 279);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(116, 42);
+            this.btnChange.TabIndex = 3;
+            this.btnChange.Text = "Изменить";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDel.Location = new System.Drawing.Point(412, 279);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(116, 42);
+            this.btnDel.TabIndex = 3;
+            this.btnDel.Text = "Удалить";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSaveFile.Location = new System.Drawing.Point(727, 279);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(116, 42);
+            this.btnSaveFile.TabIndex = 3;
+            this.btnSaveFile.Text = "Сохранить";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnOpenFile.Location = new System.Drawing.Point(727, 374);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(116, 42);
+            this.btnOpenFile.TabIndex = 3;
+            this.btnOpenFile.Text = "Загрузить";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(686, 351);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(208, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Загрузить из файла .txt";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(13, 245);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(149, 20);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Добавить в базу";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(213, 245);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(141, 20);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Редактировать";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(408, 245);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(152, 20);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Удалить из базы";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPhone.Location = new System.Drawing.Point(675, 161);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(99, 25);
+            this.lblPhone.TabIndex = 1;
+            this.lblPhone.Text = "Телефон";
+            // 
+            // lblNameCount
+            // 
+            this.lblNameCount.AutoSize = true;
+            this.lblNameCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblNameCount.Location = new System.Drawing.Point(13, 396);
+            this.lblNameCount.Name = "lblNameCount";
+            this.lblNameCount.Size = new System.Drawing.Size(246, 20);
+            this.lblNameCount.TabIndex = 1;
+            this.lblNameCount.Text = "Количество записей в базе:";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCount.Location = new System.Drawing.Point(275, 396);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(18, 20);
+            this.lblCount.TabIndex = 1;
+            this.lblCount.Text = "0";
+            // 
+            // lblProv
+            // 
+            this.lblProv.AutoSize = true;
+            this.lblProv.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblProv.Location = new System.Drawing.Point(13, 351);
+            this.lblProv.Name = "lblProv";
+            this.lblProv.Size = new System.Drawing.Size(91, 20);
+            this.lblProv.TabIndex = 1;
+            this.lblProv.Text = "Проверка";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "AnketaDB";
+            this.openFileDialog1.Filter = "Text file|*.txt|All file|*.*";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "AnketaDB";
+            this.saveFileDialog1.Filter = "Text file|*.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 450);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btnSaveFile);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblProv);
+            this.Controls.Add(this.lblNameCount);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtLname);
+            this.Controls.Add(this.lblLname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Анкета";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLname;
+        private System.Windows.Forms.TextBox txtLname;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblNameCount;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label lblProv;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace AnketaDZ
+{
+     
+    public partial class Form1 : Form
+    {
+        BindingList<Anketes> ankets = new BindingList<Anketes>();
+        public Form1()
+        {
+            InitializeComponent();            
+           
+            listBox1.DataSource = ankets;
+            listBox1.DisplayMember = "fulName";
+        }
+        public class Anketes
+        {
+            
+            public string name { get; set; }
+            public string lastName { get; set; }
+            public string email { get; set; }
+            public string phone { get; set; } 
+            public string fulName { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(IsTxtBoxEmpty())
+            {
+                MessageBox.Show("Нельзя добавить пустые значения!",
+                "Внимание",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Hand); 
+            }
+            else
+            { 
+                Anketes anketes = new Anketes();
+                anketes.name = txtName.Text;
+                anketes.lastName = txtLname.Text;
+                anketes.email = txtEmail.Text;
+                anketes.phone = txtPhone.Text;
+                anketes.fulName =$"{txtName.Text} {txtLname.Text} {txtEmail.Text} {txtPhone.Text}";
+                ankets.Add(anketes);
+                listBox1.SelectedIndex = -1;
+                ItemIndexText();
+            }  
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedIndex != -1)
+            { 
+                Anketes anketes = (Anketes)listBox1.SelectedItem;                
+
+                txtName.Text =anketes.name;
+                txtName.Select();
+                txtLname.Text = anketes.lastName;
+                txtEmail.Text = anketes.email;
+                txtPhone.Text = anketes.phone;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ankets.Add(new Anketes
+            {
+                name = "Иван",
+                lastName = "Иванов",
+                email = "ivan@mail.ru",
+                phone = "8999123456",
+                fulName = $"Иван Иванов ivan@mail.ru 8999123456"
+            });
+            listBox1.SelectedIndex = -1;
+            lblCount.Text = ankets.Count().ToString();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                lblProv.Text = fileName;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveFileDialog1.FileName;
+                lblProv.Text = fileName;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Anketes ank = (Anketes)listBox1.SelectedItem;
+            int selectedIndex = listBox1.SelectedIndex;
+            ankets.Remove(ank);            
+            //lblCount.Text = ankets.Count().ToString();
+            ItemIndexText();
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedIndex == -1)
+            { MessageBox.Show("Выберите нужную запись в списке!",
+                "Внимание",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Hand);
+            }
+            else
+            { 
+                Anketes ank = (Anketes)listBox1.SelectedItem;
+                int selectedIndex = listBox1.SelectedIndex;
+
+                ank.name = txtName.Text;
+                ank.lastName = txtLname.Text;
+                ank.email = txtEmail.Text;
+                ank.phone = txtPhone.Text;
+                ank.fulName = $"{txtName.Text} {txtLname.Text} {txtEmail.Text} {txtPhone.Text}";
+                ankets.Remove(ank);            
+                ankets.Insert(selectedIndex, ank);
+                lblCount.Text = ankets.Count().ToString();
+                listBox1.SelectedIndex = -1;
+            }
+        }
+        private void ItemIndexText()
+        {
+            if(listBox1.SelectedIndex == -1)
+            {
+                
+                txtName.Text = String.Empty; txtName.Select();
+                txtLname.Text = String.Empty;
+                txtEmail.Text = String.Empty;
+                txtPhone.Text = String.Empty;
+            }
+            lblCount.Text = ankets.Count().ToString();
+        }
+        private bool IsTxtBoxEmpty()
+        {
+            if (txtName.Text == String.Empty ||
+                txtLname.Text == String.Empty ||
+                txtEmail.Text == String.Empty ||
+                txtPhone.Text == String.Empty)
+                 return true;            
+            else return false;
+        }
+    }
+}
